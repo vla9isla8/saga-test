095fc4e76eac38ba861cb5170f8975528ead4450
vla9isla8 <vla9isla8@users.noreply.github.com>
2020-02-29 12:34:39 +0000

    deploy: 28690c617b75287fbe6f2847c5b5777b372e8a3a

---
+++ precache-manifest.9855c4b0c3b993fa51520e8d62045651.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "170bb588ffa70c95b97f86a99de9fec5",
+    "url": "/saga-test/index.html"
+  },
+  {
+    "revision": "fd3d586e6fc77521b5ce",
+    "url": "/saga-test/static/css/main.c88eb953.chunk.css"
+  },
+  {
+    "revision": "1cf933f0363c8b8909ce",
+    "url": "/saga-test/static/js/2.ac0d8e2b.chunk.js"
+  },
+  {
+    "revision": "fd3d586e6fc77521b5ce",
+    "url": "/saga-test/static/js/main.47e86fe7.chunk.js"
+  },
+  {
+    "revision": "178a0502bb436609bee6",
+    "url": "/saga-test/static/js/runtime~main.ec6d90a1.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/saga-test/static/media/logo.5d5d9eef.svg"
+  }
+]);
